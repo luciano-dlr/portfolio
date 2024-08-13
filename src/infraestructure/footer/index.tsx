1b338add5157a5fdd393a055712bc2f3a956c07f
Luciano de la Rubia <lucianodelarubia@MacBook-Air-de-Luciano.local>
2024-08-12 23:46:38 -0300

    chore(delete): Delete svg not used

--- src/infraestructure/footer/index.tsx
+++ src/infraestructure/footer/index.tsx
@@ -3,8 +3,8 @@ import LinkedInLogo from "../../assets/LinkedIn-Logo.svg";
 
 export const Footer = () => {
   return (
-    <div className="w-full bg-gray-400 sm:px-28 px-4 py-5">
-      <div className="flex flex-col sm:flex-row justify-between border-b p-5">
+    <div className="w-full bg-[#f8f8f8] sm:px-28 px-4 py-5 ">
+      <div className="flex flex-col sm:flex-row justify-between border-b p-5 ">
         <div>
           <h2 className="font-bold text-base">Luciano de la Rubia</h2>
           <div className="w-[350px] md:w-[600px]">
