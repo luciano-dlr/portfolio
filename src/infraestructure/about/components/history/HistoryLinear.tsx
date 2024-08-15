8b25ce5ab81e477383bb96785f559a22ea430af6
luciano-dlr <delarubialuciano@gmail.com>
2024-08-14 22:29:46 -0300

    chore(fixing): Margin history cards

--- src/infraestructure/about/components/history/HistoryLinear.tsx
+++ src/infraestructure/about/components/history/HistoryLinear.tsx
@@ -8,16 +8,20 @@ export const History = () => {
       className="w-full flex justify-center flex-col items-center"
     >
       <div className="rounded w-full max-w-4xl">
-        <div className="relative sm:border-l sm:pl-2 border-primary-100 dark:border-indigo-800 flex flex-col ">
+        <div className="relative sm:border-l sm:pl-2  border-primary-100 dark:border-indigo-800 flex flex-col ">
           {WorkHistory.map((exp, index) => (
-            <div key={index} className="sm:mb-10 sm:ml-4 flex items-start ">
+            //  <div key={index} className="sm:mb-10 sm:ml-4  flex items-start ">
+            <div
+              key={index}
+              className="sm:mb-10 sm:ml-4 mb-4 mx-4 flex items-start "
+            >
               <div
                 className={`absolute w-full sm:w-[20px] sm:h-[20px] bg-primary-100 dark:bg-indigo-800 rounded-full sm:-left-2.5 sm:border border-primary-100 ${
                   exp.company === "Explender SRL" ? "typing-circle" : ""
                 }`}
               ></div>
 
-              <div className="pl-4 flex flex-col gap-2 border-neutral-50 dark:border-neutral-700 border p-5 sm:m-4 rounded-lg shadow-md w-full sm:w-auto">
+              <div className="pl-4 flex flex-col gap-2 border-neutral-50  dark:border-neutral-700 border p-5 sm:m-4 rounded-lg shadow-md w-full sm:w-auto">
                 <h2 className="text-xl font-semibold dark:text-secondary-100">
                   {exp.role} - {exp.company}
                 </h2>
